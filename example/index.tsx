8cebba422fb41b4d0ed85c0d3497585d0278e712
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-09-19 19:45:12 +0200

    Improve example with theme selector + controls

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {ReactJason} from '../src/react-jason'
+import useInput from '@rooks/use-input'
+import {ReactJason, themes} from '../src/react-jason'
+
+const prefersDarkMode =
+  typeof window !== 'undefined' && typeof window.matchMedia === 'function'
+    ? window.matchMedia('(prefers-color-scheme: dark)').matches
+    : false // use light theme by default?
 
 const data = {
   string: 'Heisann',
   number: 123.45,
   boolean: true,
-  'its "cool"': 'to fly',
   null: null,
   array: ['one', 'two', 'three'],
   object: {
@@ -17,22 +22,64 @@ const data = {
   keyedArray: [
     {
       _key: 'abc',
-      _type: 'image',
+      _type: 'span',
       text: 'Coolio',
     },
     {
       _key: 'xyz',
-      _type: 'image',
+      _type: 'span',
       text: 'Yes',
     },
   ],
 }
 
+const themeList = {
+  '': 'VS Code (auto)',
+  anOldHopeClassic: 'An Old Hope Classic',
+  github: 'GitHub',
+  monokai: 'Monokai',
+  monokaiSublime: 'Monokai Sublime',
+  vscodeDark: 'VS Code (Dark)',
+  vscodeLight: 'VS Code (Light)',
+}
+
 const App = () => {
+  const {value: selectedTheme, onChange: onChangeTheme} = useInput('')
+  const [quoteAttributes, setQuoteAttributes] = React.useReducer(prev => !prev, true)
+
+  let theme = prefersDarkMode ? themes.vscodeDark : themes.vscodeLight
+  if (selectedTheme !== '') {
+    theme = themes[selectedTheme]
+  }
+
   return (
-    <div>
-      <ReactJason value={data} />
-    </div>
+    <>
+      <h1>
+        <a href="https://github.com/rexxars/react-jason">react-jason</a> demo
+      </h1>
+
+      <div id="controls">
+        <select id="theme" onChange={onChangeTheme as any} defaultValue={selectedTheme}>
+          {Object.keys(themeList).map(themeName => (
+            <option key={themeName} value={themeName}>
+              {themeList[themeName]}
+            </option>
+          ))}
+        </select>
+
+        <label>
+          Quote properties
+          <input
+            type="checkbox"
+            name="Quote props"
+            onChange={setQuoteAttributes}
+            checked={quoteAttributes}
+          />
+        </label>
+      </div>
+
+      <ReactJason value={data} theme={theme} quoteAttributes={quoteAttributes} />
+    </>
   )
 }
 
